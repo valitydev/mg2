4826dd0cc59fbff969d381343c09dec6f4857641
ndiezel0 <ndiezel0@gmail.com>
2022-04-28 19:13:07 +0300

    TD-163: Update machinegun-core (#16)

--- src/machinegun_pulse.erl
+++ src/machinegun_pulse.erl
@@ -30,17 +30,7 @@
     | mg_core_consuela_pulse_adapter:beat()
     | mg_core_queue_scanner:beat()
     | #woody_event{}
-    | #woody_request_handle_error{}
-    %% TODO TD-229: Move it to `mg_core_pulse:beat()`
-    | #mg_core_scheduler_search_success{}
-    | #mg_core_riak_client_get_start{}
-    | #mg_core_riak_client_get_finish{}
-    | #mg_core_riak_client_put_start{}
-    | #mg_core_riak_client_put_finish{}
-    | #mg_core_riak_client_search_start{}
-    | #mg_core_riak_client_search_finish{}
-    | #mg_core_riak_client_delete_start{}
-    | #mg_core_riak_client_delete_finish{}.
+    | #woody_request_handle_error{}.
 
 -type options() :: #{
     hay_options => machinegun_pulse_hay:options(),
